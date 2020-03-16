39020b3523549485f9cb78ddc8d6237c9e9e50f1
ca0v <coreyalix@hotmail.com>
2020-03-16 06:34:26 -0400

    dblclick to import parcel

--- app/WhereAmI.js
+++ app/WhereAmI.js
@@ -5,31 +5,40 @@ export class WhereAmI {
         const source = (this.source = new ol.source.Vector());
         const penLayer = new ol.layer.Vector({ source: source, visible: true });
         options.map.addLayer(penLayer);
-        this.currentPosition = new ol.Feature();
+        this.activeFeature = new ol.Feature();
         const fill = new ol.style.Fill({ color: "red" });
         const stroke = new ol.style.Stroke({ color: "white", width: 1 });
         const circle = new ol.style.Circle({ radius: 10, stroke, fill });
         const style = new ol.style.Style({ image: circle });
-        this.currentPosition.setStyle(style);
-        this.source.addFeature(this.currentPosition);
+        this.activeFeature.setStyle(style);
+        this.source.addFeature(this.activeFeature);
     }
     start() {
         navigator.geolocation.watchPosition(position => {
             this.plotPosition(position);
             this.savePosition(position);
+            this.currentPosition = position;
         });
     }
     plotPosition(position) {
         const { coords, timestamp } = position;
         const { latitude, longitude } = coords;
-        const location = new ol.geom.Point([longitude, latitude]);
-        const mapLocation = location.transform("EPSG:4326", "EPSG:3857");
-        this.options.map.getView().setCenter(mapLocation.getFirstCoordinate());
+        const mapLocation = this.transform(longitude, latitude);
+        {
+            let center = mapLocation.getFirstCoordinate();
+            //center = this.transform(-82.3525, 34.87788).getFirstCoordinate();
+            this.options.map.getView().setCenter(center);
+        }
         const feature = new ol.Feature(mapLocation);
         this.source.addFeature(feature);
-        this.currentPosition.setGeometry(feature.getGeometry());
-        this.source.removeFeature(this.currentPosition);
-        this.source.addFeature(this.currentPosition);
+        this.activeFeature.setGeometry(feature.getGeometry());
+        this.source.removeFeature(this.activeFeature);
+        this.source.addFeature(this.activeFeature);
+    }
+    transform(longitude, latitude) {
+        const location = new ol.geom.Point([longitude, latitude]);
+        const mapLocation = location.transform("EPSG:4326", "EPSG:3857");
+        return mapLocation;
     }
     savePosition(position) {
         this.options.storage.savePosition(position);
--- app/WhereAmI.ts
+++ app/WhereAmI.ts
@@ -3,7 +3,8 @@ const ol = globalThis.ol;
 
 export class WhereAmI {
   private source: ol.source.Vector;
-  private currentPosition: ol.Feature;
+  private activeFeature: ol.Feature;
+  currentPosition: Position|undefined;
 
   constructor(
     private options: {
@@ -14,36 +15,50 @@ export class WhereAmI {
     const source = (this.source = new ol.source.Vector());
     const penLayer = new ol.layer.Vector({ source: source, visible: true });
     options.map.addLayer(penLayer);
-    this.currentPosition = new ol.Feature();
+    this.activeFeature = new ol.Feature();
     const fill = new ol.style.Fill({ color: "red" });
     const stroke = new ol.style.Stroke({ color: "white", width: 1 });
     const circle = new ol.style.Circle({ radius: 10, stroke, fill });
-    const style = new ol.style.Style({image:circle});
-    this.currentPosition.setStyle(style);
-    this.source.addFeature(this.currentPosition);
+    const style = new ol.style.Style({ image: circle });
+    this.activeFeature.setStyle(style);
+    this.source.addFeature(this.activeFeature);
   }
 
   start() {
     navigator.geolocation.watchPosition(position => {
       this.plotPosition(position);
       this.savePosition(position);
+      this.currentPosition = position;
     });
   }
+
   plotPosition(position: Position): void {
     const { coords, timestamp } = position;
     const { latitude, longitude } = coords;
+    const mapLocation = this.transform(longitude, latitude);
+
+    {
+      let center = mapLocation.getFirstCoordinate();
+      //center = this.transform(-82.3525, 34.87788).getFirstCoordinate();
+      this.options.map.getView().setCenter(center);
+    }
+
+    const feature = new ol.Feature(mapLocation);
+    this.source.addFeature(feature);
+    this.activeFeature.setGeometry(feature.getGeometry());
+    this.source.removeFeature(this.activeFeature);
+    this.source.addFeature(this.activeFeature);
+  }
+
+  private transform(longitude: number, latitude: number) {
     const location = new ol.geom.Point([longitude, latitude]);
     const mapLocation = location.transform(
       "EPSG:4326",
       "EPSG:3857"
     ) as ol.geom.Point;
-    this.options.map.getView().setCenter(mapLocation.getFirstCoordinate());
-    const feature = new ol.Feature(mapLocation);
-    this.source.addFeature(feature);
-    this.currentPosition.setGeometry(feature.getGeometry());
-    this.source.removeFeature(this.currentPosition);
-    this.source.addFeature(this.currentPosition);
+    return mapLocation;
   }
+
   savePosition(position: Position) {
     this.options.storage.savePosition(position);
   }
